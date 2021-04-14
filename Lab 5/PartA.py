e3a79117a026a4708c0c2adb9a49fbffb91c60ea
greatcan11 <greatcan11@gmail.com>
2021-04-14 11:26:09 -0400

    Done with PartA

--- Lab 5/PartA.py
+++ Lab 5/PartA.py
@@ -6,11 +6,15 @@ import imageio
 # Function to determine if an image would be "red"
 def is_red(img):
     i = imageio.imread(img)
-    min_R = np.amin(i[:,:,0], axis = 0)
-    if(np.amin(min_R) > 15):
-        print("The ",img, "picture has red.")
+    red = i[:,:,0]
+    min_red = np.amin(red, axis = 0)
+    max_red = np.amax(np.amax(red), axis = 0)
+    avg_red = np.average(np.average(red, axis = 0))
+    if(np.amin(min_red) > 15):
+        print("The ",img, "picture has a red tint.", end ='')
     else:
-        print("The ",img, "picture does not have red.")
+        print("The ",img, "picture does not have a red tint.", end ='')
+    print("The average pixel value for red is ", int(avg_red),"and the highest value is ",max_red,".")
 
 # Sample images
 is_red('red.jpg')
