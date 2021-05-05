8069cd84f1a6ba899cdc6e921ec20ae495c890da
greatcan11 <greatcan11@gmail.com>
2021-05-05 12:07:02 -0400

    Finally finished cardswipe

--- Final Project/cardswipe.py
+++ Final Project/cardswipe.py
@@ -2,24 +2,21 @@ import time
 import board
 import busio
 import adafruit_mpr121
-# import datetime
-# from datetime import timedelta
-# from datetime import time
 
+# Initialize connections
 i2c = busio.I2C(board.SCL, board.SDA)
 mpr121 = adafruit_mpr121.MPR121(i2c)
 
+# Initialize variables
 start_time = 0
 end_time = 0
 swipe_time_sec = 0
+
 while(not(swipe_time_sec>1 and swipe_time_sec<3)):
-    if mpr121[0].value:
-        start_time = time.perf_counter()
+    start_time = time.perf_counter()
     while (mpr121[0].value):
         print("still touching")
-    if not (mpr121[0].value):
-        end_time = time.perf_counter()
+    end_time = time.perf_counter()
     swipe_time_sec = end_time - start_time
     print(swipe_time_sec)
 print("done")
-time.sleep(0.25)  # Small delay to keep from spamming output messages.
